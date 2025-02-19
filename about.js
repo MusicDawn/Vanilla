29c991b7f6c8e373624d88a198421e350557308e
MusicDawn <panagiotis.d.kostakis@gmail.com>
2025-02-19 18:01:14 +0200

    JS part 2

---
+++ about.js
@@ -0,0 +1,4 @@
+const about = `<h2>About</h2>`
+
+//The line below brings home to global scope!
+window.about = about
---
+++ home.js
@@ -0,0 +1,4 @@
+const home = `<h2>Home</h2>`
+
+//The line below brings home to global scope!
+window.home = home
