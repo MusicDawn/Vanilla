0478c347570cccb6808db496c63278d1911600ce
MusicDawn <panagiotis.d.kostakis@gmail.com>
2025-06-18 14:45:30 +0300

    Progressing Ep 10

---
+++ Avue/src/views/ReactiveView.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+export default {
+    data() {
+        return {
+            user: {
+                name: "Panos",
+                city: "Melanes"
+            },
+            nameBool: true
+        }
+    },
+    methods: {
+        toggleName(){
+            this.nameBool = !this.nameBool
+            this.nameBool ? this.user.name = "Panos" : this.user.name = "Sasa Mpasta"
+            return this.user.name
+        }
+    }
+}
+</script>
+
+<template>
+    <div id="title">
+        <h1>Reactive View page</h1>
+        <h2>{{ user.name }}</h2>
+        <button @click="toggleName">Click Me!</button>
+    </div>
+</template>
+
+
+<style scoped>
+#title {
+    background-color: var(--color-background-mute);
+}
+</style>
