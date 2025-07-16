b08d44cdb1adaa1b4c67fa2137c6da0e155f7301
MusicDawn <panagiotis.d.kostakis@gmail.com>
2025-07-16 23:41:53 +0300

    Progressing 12

--- Avue/src/views/PlaygroundView.vue
+++ Avue/src/views/PlaygroundView.vue
@@ -1,9 +1,33 @@
 <script lang="ts">
+import { useConfigStore } from '@/stores/config';
+import { defineComponent } from 'vue';
+import CompPlayground from '@/components/CompPlayground.vue';
+import OptPlayground from '@/components/OptPlayground.vue';
 
+
+export default defineComponent({
+    computed: {
+        currentComponent() {
+            const configStore = useConfigStore()
+            return configStore.useCompositionAPI ? 'CompositionAPIComponent' : 'OptionsAPIComponent'
+        }
+    },
+    components: {
+        CompPlayground,
+        OptPlayground
+    }
+})
 </script>
 
 <template>
-    <div>
-        Playground
+    <div id="title">
+        <h1>Plauground Page</h1>
+        <component :is="currentComponent"></component>
     </div>
 </template>
+
+<style scoped>
+#title {
+    background-color: var(--color-background-mute);
+}
+</style>
